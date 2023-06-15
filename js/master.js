a4318b8190b633924bf5eb8ad505be9e99e4bb40
smy <smy.family77@gmail.com>
2023-06-15 14:36:17 +0300

    0.0.9

--- js/master.js
+++ js/master.js
@@ -6,3 +6,30 @@ menu.onclick = function () {
     links.style.height = "0px";
   }
 };
+
+// work
+// https://muhammed-abdelaleam.github.io/me/
+fetch("./js/work.json")
+  .then((data) => data.json())
+  .then((data) => {
+    for (const key in data) {
+      let pro = data[key];
+      let div = document.createElement("div");
+      div.className = "card";
+      div.innerHTML = `
+          <div class="photo">
+            <img
+              src="${pro.photo}"
+              alt="work image"
+            />
+          </div>
+          <div class="label" data-level="${pro.level}">${pro.level}</div>
+          <div class="title">${pro.name}</div>
+          <div class="des">${pro.des}</div>
+      `;
+      div.onclick = function () {
+        location.href = pro.link;
+      };
+      workE.appendChild(div);
+    }
+  });
